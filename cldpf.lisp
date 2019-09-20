f1c87a701121051f7db22bce0c97b781d94bb1b4
myaosato <tetu60u@yahoo.co.jp>
2019-09-20 22:36:07 +0900

    cldpf/listに対する依存関係追加

--- cldpf.lisp
+++ cldpf.lisp
@@ -2,6 +2,8 @@
                              (:import-from :cldpf/item :make-item-file)
                              (:import-from :cldpf/feed :make-feed)
                              (:import-from :cldpf/program :make-program)
+                             (:import-from :cldpf/list :read-item-file 
+                              :read-program-file)
                              (:export) (:intern))
 (in-package :cldpf/cldpf)
 ;;don't edit above
